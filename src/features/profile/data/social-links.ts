ecc29e35a2d26d0a94df54ab1760ba70d9bec596
Kaushik <veloxineology@gmail.com>
2025-04-22 12:43:04 +0530

    changed socials

--- src/features/profile/data/social-links.ts
+++ src/features/profile/data/social-links.ts
@@ -2,45 +2,33 @@ import { SocialLink } from "../types/social-links";
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    icon: "/images/link-icons/linkedin.webp",
-    title: "LinkedIn",
-    description: "ncdai",
-    href: "https://linkedin.com/in/ncdai",
-  },
-  {
     icon: "/images/link-icons/github.webp",
     title: "GitHub",
-    description: "ncdai",
-    href: "https://github.com/ncdai",
-  },
-  {
-    icon: "/images/link-icons/dailydev.webp",
-    title: "daily.dev",
-    description: "@ncdai",
-    href: "https://app.daily.dev/ncdai",
-  },
-  {
-    icon: "/images/link-icons/x.webp",
-    title: "X",
-    description: "@iamncdai",
-    href: "https://x.com/iamncdai",
-  },
-  {
-    icon: "/images/link-icons/zalo.webp",
-    title: "Zalo",
-    description: "Quaric",
-    href: "https://zalo.me/2353934240045322830",
-  },
-  {
-    icon: "/images/link-icons/telegram.webp",
-    title: "Telegram",
-    description: "@ncdai",
-    href: "https://t.me/ncdai",
+    description: "veloxineology",
+    href: "https://github.com/veloxineology",
   },
   {
     icon: "/images/link-icons/youtube.webp",
     title: "YouTube",
-    description: "@ncdai",
-    href: "https://www.youtube.com/@ncdai",
+    description: "@kaushikieee",
+    href: "https://www.youtube.com/@kaushikieee",
+  },
+  {
+    icon: "/images/link-icons/instagram.webp",
+    title: "Instagram",
+    description: "@kaushikieee",
+    href: "https://www.instagram.com/kaushikieee",
+  },
+  {
+    icon: "/images/link-icons/telegram.webp",
+    title: "Telegram",
+    description: "@kaushikieee",
+    href: "https://t.me/kaushikieee",
+  },
+  {
+    icon: "/images/link-icons/x.webp",
+    title: "X",
+    description: "@kaushikieee",
+    href: "https://x.com/kaushikieee",
   },
 ];
